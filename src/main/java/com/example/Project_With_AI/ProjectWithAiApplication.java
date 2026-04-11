99956faed8bea51b529c4b264f91ea5b76e8a6a0
nurubel1819 <rubel.18cse255@bsmrstu.edu.bd>
2026-04-11 16:14:24 +0600

    Initial commit without db connection

---
+++ src/main/java/com/example/Project_With_AI/ProjectWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_With_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project_With_AI/ProjectWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_With_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
